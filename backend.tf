d3cd14d3500d6a1a585ee3a5ac5cec516ff79fee
Vital Slepiankou <vital_slepiankou@epam.com>
2024-09-28 15:47:38 +0300

    remove varaibles

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
   backend "s3" {
     bucket  = "rs-school-vital-slepiankou-1"  # Name of the S3 bucket to store Terraform state files
     key     = "terraform/state"               # Path within the S3 bucket where the state file will be stored
-    region  = var.aws_region                  # AWS region of the S3 bucket for state storage
+    region  = "eu-central-1"                  # AWS region of the S3 bucket for state storage
   }
 }
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to launch servers"
-  type        = string
-  default     = "eu-central-1"
-}
